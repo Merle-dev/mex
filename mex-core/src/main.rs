2d52a491d7841afcd224df26cf117b18bc8721ef
merle <merleesser.dev@gmail.com>
2025-11-04 23:42:36 +0100

    worked on async jobs

--- mex-core/src/main.rs
+++ mex-core/src/main.rs
@@ -1,3 +1,38 @@
+use futures::{
+    StreamExt,
+    future::{BoxFuture, FutureExt},
+    stream::FuturesUnordered,
+};
+
+#[derive(Debug)]
+enum Mode {
+    Normal,
+    Insert,
+    Select,
+}
+
+struct Context {
+    mode: Mode,
+}
+
+type Callback = Box<dyn FnOnce(&mut Context) + Send>;
+type Job = BoxFuture<'static, Option<Callback>>;
+
+struct Jobs {
+    list: FuturesUnordered<Job>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut jobs = Jobs {
+        list: FuturesUnordered::new(),
+    };
+    let call = Box::pin(async {
+        let cb: Callback = Box::new(|ctx: &mut Context| println!("last"));
+        Some(cb)
+    });
+    jobs.list.push(call);
+    let mut cx = Context { mode: Mode::Normal };
+    while let Some(callback) = smol::block_on(async { jobs.list.next().await.flatten() }) {
+        callback(&mut cx);
+    }
 }
